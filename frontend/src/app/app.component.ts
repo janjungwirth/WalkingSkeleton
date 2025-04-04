cab94128113dfcea6f238734af76a73b3d22c867
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2025-04-04 15:36:33 +0200

    Merge pull request #3
    * feat: adds prettier
    
    * feat: adds prettier

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatFormField, MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatButton} from '@angular/material/button';
-import {MatCard, MatCardActions, MatCardContent, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
-import {HttpClient} from '@angular/common/http';
+import { MatFormField, MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
-import {MatCheckbox} from '@angular/material/checkbox';
-import {NgIf} from '@angular/common';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatFormField, FormsModule, MatFormFieldModule, MatInputModule, MatButton, MatCardContent, MatCard, MatCardTitle, MatCardSubtitle, HttpClientModule, ReactiveFormsModule, MatCardActions, MatCheckbox, NgIf],
+  imports: [
+    RouterOutlet,
+    MatFormField,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButton,
+    MatCardContent,
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatCardActions,
+    MatCheckbox,
+    NgIf,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
@@ -23,17 +39,20 @@ export class AppComponent {
   showSuccess = false;
   showError = false;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
     this.form = this.fb.group({
       vorname: ['', Validators.required],
       nachname: ['', Validators.required],
       date: ['', Validators.required],
       location: this.fb.array([
         this.createLocationGroup(), // index 0 - "Von"
-        this.createLocationGroup()  // index 1 - "Zu"
+        this.createLocationGroup(), // index 1 - "Zu"
       ]),
       amount: ['', Validators.required],
-      packaging: [true]
+      packaging: [true],
     });
   }
 
@@ -41,7 +60,7 @@ export class AppComponent {
     return this.fb.group({
       plz: ['', Validators.required],
       city: ['', Validators.required],
-      street: ['', Validators.required]
+      street: ['', Validators.required],
     });
   }
 
@@ -54,14 +73,14 @@ export class AppComponent {
     const payload = {
       person: {
         vorname: formValue.vorname,
-        nachname: formValue.nachname
+        nachname: formValue.nachname,
       },
       date: formValue.date,
       location: formValue.location,
       amount: formValue.amount,
       additional_info: {
-        packaging: formValue.packaging
-      }
+        packaging: formValue.packaging,
+      },
     };
 
     this.http.post('http://localhost:8080/umzug', payload).subscribe({
@@ -73,7 +92,7 @@ export class AppComponent {
       error: () => {
         this.showError = true;
         this.showSuccess = false;
-      }
+      },
     });
 
     console.log(payload);
